3a0bfe4f6347513ad2ffcd65821b96d684ad4c34
isaisebastian <62181275+isaisebastian@users.noreply.github.com>
2021-05-30 16:10:38 +0300

    adding my new project files

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Sebastian Isai',
+		username: 'Shaun',
+		email: 'shaun@robot.com'
+	},
+	{
+		id: 2,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'Sincere@april.biz'
+	},
+	{
+		id: 3,
+		name: 'Ervin Howell',
+		username: 'Antonette',
+		email: 'Shanna@melissa.tv'
+	},
+	{
+		id: 4,
+		name: 'Melissa Sally',
+		username: 'Meli',
+		email: 'Sally@winner.tv'
+	}
+]
